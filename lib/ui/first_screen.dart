d94d90b51cc0c091ddab9b864924deba18b99dd4
spyasdza <32991920+spyasdza@users.noreply.github.com>
2019-02-18 11:22:23 +0700

    Lab week 5

--- lib/ui/first_screen.dart
+++ lib/ui/first_screen.dart
@@ -1,25 +1,81 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'detail_screen.dart';
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:image_picker/image_picker.dart';
 
-class FirstScreen extends StatelessWidget{
+class FirstScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return FirstScreenState();
+  }
+}
+
+class FirstScreenState extends State {
+  List<String> _passengers = <String>["", "1", "2", "3", "4", "5"];
+  String _passenger = "1";
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+          _image = image;
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera),
+        onPressed: (){
+          getImage();
+        },
+      ),
       appBar: AppBar(
         title: Text("First Screen"),
       ),
-      body: Center(
-        child: FlatButton(
-          child: Text("go to detail"),
-          onPressed: (){
-          Navigator.push( 
-            context,
-            MaterialPageRoute(
-              builder: (context) => DetailScreen(title: "Hello"),
+      body: ListView(
+        children: <Widget>[
+          _image == null 
+          ? Text("No images selected")
+          : Image.file(_image),
+
+          InputDecorator(
+            decoration: InputDecoration(
+                icon: Icon(Icons.people), labelText: "Passenger"),
+            isEmpty: _passenger == "",
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                isDense: true,
+                value: _passenger,
+                onChanged: (String value) {
+                  setState(() {
+                    _passenger = value;
+                  });
+                },
+                items: _passengers.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
               ),
-            );
-          },
-        ),
+            ),
+          ),
+          DateTimePickerFormField(
+            inputType: InputType.date,
+            format: DateFormat("yyyy-MM-dd"),
+            editable: true,
+            decoration: InputDecoration(
+                labelText: "Date", icon: Icon(Icons.calendar_today)),
+            onChanged: (value) {},
+          ),
+        ],
       ),
     );
   }
